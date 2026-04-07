35d0629894429be6da4e96f1bfe1d398a3bb29f0
lucasquinaliasptech <lucas.quinalia@sptech.school>
2026-04-07 09:44:36 -0300

    API de aquisição de dados do sensor e armazenamento no BD

---
+++ Arduino/dat-acqu-ino/main.js
@@ -0,0 +1,123 @@
+// importa os bibliotecas necessários
+const serialport = require('serialport');
+const express = require('express');
+const mysql = require('mysql2');
+
+// constantes para configurações
+const SERIAL_BAUD_RATE = 9600;
+const SERVIDOR_PORTA = 3300;
+
+// habilita ou desabilita a inserção de dados no banco de dados
+const HABILITAR_OPERACAO_INSERIR = true;
+
+// função para comunicação serial
+const serial = async (
+    valoresSensorTemperatura,
+    valoresSensorUmidade
+) => {
+
+    // conexão com o banco de dados MySQL
+    let poolBancoDados = mysql.createPool(
+        {
+            host: 'localhost',
+            user: 'user_insert',
+            password: 'mootech',
+            database: 'mootech',
+            port: 3306
+        }
+    ).promise();
+
+    // lista as portas seriais disponíveis e procura pelo Arduino
+    const portas = await serialport.SerialPort.list();
+    const portaArduino = portas.find((porta) => porta.vendorId == 2341 && porta.productId == 43);
+    if (!portaArduino) {
+        throw new Error('O arduino não foi encontrado em nenhuma porta serial');
+    }
+
+    // configura a porta serial com o baud rate especificado
+    const arduino = new serialport.SerialPort(
+        {
+            path: portaArduino.path,
+            baudRate: SERIAL_BAUD_RATE
+        }
+    );
+
+    // evento quando a porta serial é aberta
+    arduino.on('open', () => {
+        console.log(`A leitura do arduino foi iniciada na porta ${portaArduino.path} utilizando Baud Rate de ${SERIAL_BAUD_RATE}`);
+    });
+
+    // processa os dados recebidos do Arduino
+    arduino.pipe(new serialport.ReadlineParser({ delimiter: '\r\n' })).on('data', async (data) => {
+        console.log(data);
+        const valores = data.split(';');
+        const sensorTemperatura = parseFloat(valores[0]);
+        const sensorUmidade = parseFloat(valores[1]);
+
+        // armazena os valores dos sensores nos arrays correspondentes
+        valoresSensorTemperatura.push(sensorTemperatura);
+        valoresSensorUmidade.push(sensorUmidade);
+
+        // insere os dados no banco de dados (se habilitado)
+        if (HABILITAR_OPERACAO_INSERIR) {
+
+            // este insert irá inserir os dados na tabela "medida"
+            await poolBancoDados.execute(
+                'INSERT INTO registro (id_sensor_temperatura_umidade, temperatura, umidade) VALUES (?, ?, ?)',
+                [1, sensorTemperatura, sensorUmidade]
+            );
+            console.log("valores inseridos no banco: ", sensorTemperatura, " e ", sensorUmidade);
+            
+        }
+
+    });
+
+    // evento para lidar com erros na comunicação serial
+    arduino.on('error', (mensagem) => {
+        console.error(`Erro no arduino (Mensagem: ${mensagem}`)
+    });
+}
+
+// função para criar e configurar o servidor web
+const servidor = (
+    valoresSensorTemperatura,
+    valoresSensorUmidade
+) => {
+    const app = express();
+
+    // configurações de requisição e resposta
+    app.use((request, response, next) => {
+        response.header('Access-Control-Allow-Origin', '*');
+        response.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+        next();
+    });
+
+    // inicia o servidor na porta especificada
+    app.listen(SERVIDOR_PORTA, () => {
+        console.log(`API executada com sucesso na porta ${SERVIDOR_PORTA}`);
+    });
+
+    // define os endpoints da API para cada tipo de sensor
+    app.get('/sensores/temperatura', (_, response) => {
+        return response.json(valoresSensorTemperatura);
+    });
+    app.get('/sensores/umidade', (_, response) => {
+        return response.json(valoresSensorUmidade);
+    });
+}
+
+// função principal assíncrona para iniciar a comunicação serial e o servidor web
+(async () => {
+    const valoresSensorTemperatura = [];
+    const valoresSensorUmidade = [];
+
+    await serial(
+        valoresSensorTemperatura,
+        valoresSensorUmidade
+    );
+
+    servidor(
+        valoresSensorTemperatura,
+        valoresSensorUmidade
+    );
+})();
