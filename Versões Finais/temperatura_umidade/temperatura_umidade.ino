2920a9e2c4b14b50953e2be7b0f6f1c830fd4959
YasminEloitaNascimento <yasmin.nascimento@sptech.school>
2026-04-20 18:36:54 -0300

    Cadastro/Login Com inserção no BD

---
+++ Versões Finais/temperatura_umidade/temperatura_umidade.ino
@@ -0,0 +1,36 @@
+#include "DHT.h";
+
+#define TIPO_SENSOR DHT11
+const int PINO_SENSOR_DHT11 = A0;
+
+DHT sensorDHT(PINO_SENSOR_DHT11, TIPO_SENSOR);
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  sensorDHT.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float umidade = sensorDHT.readHumidity();
+  float temperatura = sensorDHT.readTemperature();
+
+  if (isnan(temperatura) || isnan(umidade)) {
+    Serial.println("Erro ao ler o sensor");
+  } else {
+   // Serial.print("Umidade ");
+    Serial.print(umidade);
+   // Serial.print(" % ");
+   // Serial.print("Temperatura ");
+    Serial.print(";");
+
+    Serial.print(temperatura);
+   // Serial.print(" °c");
+    Serial.println(";");
+ 
+  }
+
+  delay(1000);
+}
