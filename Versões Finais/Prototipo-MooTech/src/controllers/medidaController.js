bebbe95dd6ee6804892da931d9b647977176a246
lucas-britto-dev <lucas.cruz@sptech.school>
2026-04-20 18:12:51 -0300

    refactor: alteração no src/medida e dashboard.html de acordo com a regra de negócio

--- Versões Finais/Prototipo-MooTech/src/controllers/medidaController.js
+++ Versões Finais/Prototipo-MooTech/src/controllers/medidaController.js
@@ -1,14 +1,35 @@
 var medidaModel = require("../models/medidaModel");
 
-function buscarUltimasMedidas(req, res) {
+function buscarTemperatura(req, res) {
 
-    const limite_linhas = 7;
+    const limite_linhas = 10;
 
-    var idAquario = req.params.idAquario;
+    var idTanque = req.params.idTanque;
 
     console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
 
-    medidaModel.buscarUltimasMedidas(idAquario, limite_linhas).then(function (resultado) {
+    medidaModel.buscarTemperatura(idTanque, limite_linhas).then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+function buscarUmidade(req, res) {
+
+    const limite_linhas = 10;
+
+    var idTanque = req.params.idTanque;
+
+    console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
+
+    medidaModel.buscarUmidade(idTanque, limite_linhas).then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
@@ -22,13 +43,32 @@ function buscarUltimasMedidas(req, res) {
 }
 
 
-function buscarMedidasEmTempoReal(req, res) {
+function buscarTemperaturaEmTempoReal(req, res) {
 
-    var idAquario = req.params.idAquario;
+    var idTanque = req.params.idTanque;
 
     console.log(`Recuperando medidas em tempo real`);
 
-    medidaModel.buscarMedidasEmTempoReal(idAquario).then(function (resultado) {
+    medidaModel.buscarMedidasEmTempoReal(idTanque).then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+function buscarUmidadeEmTempoReal(req, res) {
+
+    var idTanque = req.params.idAquario;
+
+    console.log(`Recuperando medidas em tempo real`);
+
+    medidaModel.buscarUmidadeEmTempoReal(idTanque).then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
@@ -42,7 +82,9 @@ function buscarMedidasEmTempoReal(req, res) {
 }
 
 module.exports = {
-    buscarUltimasMedidas,
-    buscarMedidasEmTempoReal
+    buscarTemperatura,
+    buscarUmidade,
+    buscarTemperaturaEmTempoReal,
+    buscarUmidadeEmTempoReal
 
 }
--- Versões Finais/Prototipo-MooTech/src/routes/medidas.js
+++ Versões Finais/Prototipo-MooTech/src/routes/medidas.js
@@ -3,12 +3,20 @@ var router = express.Router();
 
 var medidaController = require("../controllers/medidaController");
 
-router.get("/ultimas/:idAquario", function (req, res) {
-    medidaController.buscarUltimasMedidas(req, res);
+router.get("/ultimas/:idTanque", function (req, res) {
+    medidaController.buscarTemperatura(req, res);
 });
 
-router.get("/tempo-real/:idAquario", function (req, res) {
-    medidaController.buscarMedidasEmTempoReal(req, res);
+router.get("/ultimas/:idTanque", function (req, res) {
+    medidaController.buscarUmidade(req, res);
+});
+
+router.get("/tempo-real/:idTanque", function (req, res) {
+    medidaController.buscarTemperaturaEmTempoReal(req, res);
+})
+
+router.get("/tempo-real/:idTanque", function (req, res) {
+    medidaController.buscarUmidadeEmTempoReal(req, res);
 })
 
 module.exports = router;
